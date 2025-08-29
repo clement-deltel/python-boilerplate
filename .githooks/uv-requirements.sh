4ac2f9f593d3d8538e274c5299b66c06e7133444
Clement Deltel <38040315+clement-deltel@users.noreply.github.com>
2025-08-29 15:10:34 -0500

    🐛 fix: update uv requirements hook

--- .githooks/uv-requirements.sh
+++ .githooks/uv-requirements.sh
@@ -5,7 +5,7 @@ if [[ ! -d "requirements" ]]; then
 fi
 
 uv export --format requirements-txt --no-default-groups --no-emit-project --output-file requirements/requirements.txt
-uv export --format requirements-txt --no-default-groups --no-emit-project --output-file --group dev --group lint requirements/requirements-dev.txt
-uv export --format requirements-txt --no-default-groups --no-emit-project --output-file --group test requirements/requirements-test.txt
+uv export --format requirements-txt --no-default-groups --no-emit-project --output-file requirements/requirements-dev.txt --group dev --group lint
+uv export --format requirements-txt --no-default-groups --no-emit-project --output-file requirements/requirements-test.txt --group test
 
 git add requirements/

c64cb1d7d205f3cbc4e9677363da9d143dea4271
Clement Deltel <38040315+clement-deltel@users.noreply.github.com>
2026-01-20 16:15:56 -0600

    ➕ build: introduce cve fixes dependency group

--- .githooks/uv-requirements.sh
+++ .githooks/uv-requirements.sh
@@ -4,8 +4,8 @@ if [[ ! -d "requirements" ]]; then
   mkdir -p requirements
 fi
 
-uv export --format requirements-txt --no-default-groups --no-emit-project --output-file requirements/requirements.txt
-uv export --format requirements-txt --no-default-groups --no-emit-project --output-file requirements/requirements-dev.txt --group dev --group lint
-uv export --format requirements-txt --no-default-groups --no-emit-project --output-file requirements/requirements-test.txt --group test
+uv export --format requirements-txt --no-default-groups --no-emit-project --output-file requirements/requirements.txt --group cve
+uv export --format requirements-txt --no-default-groups --no-emit-project --output-file requirements/requirements-dev.txt --group cve --group dev --group lint --group typing
+uv export --format requirements-txt --no-default-groups --no-emit-project --output-file requirements/requirements-test.txt --group cve --group test
 
 git add requirements/
